fa65f228783c7e3f47adeb9a923879f131583b2b
Suchitra Swain <suchitraswain.2012gmail.com>
2024-08-21 23:25:28 +0200

    Onboarding screen

---
+++ app/(root)/(tabs)/chat.tsx
@@ -0,0 +1,12 @@
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const Chat = () => {
+  return (
+    <SafeAreaView>
+      <Text>Hellooooooo</Text>
+    </SafeAreaView>
+  );
+};
+
+export default Chat;

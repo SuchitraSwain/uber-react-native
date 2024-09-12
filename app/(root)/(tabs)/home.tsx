08b75c5adb4a722cda5798afb8825407f90c88c7
Suchitra Swain <suchitraswain.2012gmail.com>
2024-09-12 02:40:58 +0200

    final comment

--- app/(root)/(tabs)/home.tsx
+++ app/(root)/(tabs)/home.tsx
@@ -1,127 +1,44 @@
+import { useUser } from "@clerk/clerk-expo";
+import { useAuth } from "@clerk/clerk-expo";
+import * as Location from "expo-location";
+import { router } from "expo-router";
+import { useState, useEffect } from "react";
+import {
+  Text,
+  View,
+  TouchableOpacity,
+  Image,
+  FlatList,
+  ActivityIndicator,
+} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
 import GoogleTextInput from "@/components/GoogleTextInput";
 import Map from "@/components/Map";
 import RideCard from "@/components/RideCard";
 import { icons, images } from "@/constants";
+import { useFetch } from "@/lib/fetch";
 import { useLocationStore } from "@/store";
-import { useAuth, useUser } from "@clerk/clerk-expo";
-import { router } from "expo-router";
-import { useEffect, useState } from "react";
-import {
-  ActivityIndicator,
-  FlatList,
-  Image,
-  Text,
-  TouchableOpacity,
-  View,
-} from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
-import * as Location from "expo-location";
+import { Ride } from "@/types/type";
 
-export default function Page() {
+const Home = () => {
   const { user } = useUser();
   const { signOut } = useAuth();
-  const { setUserLocation, setDestinationLocation } = useLocationStore();
-  const [hasPermission, setHasPermission] = useState<boolean>(false);
 
-  const loading = false;
+  const { setUserLocation, setDestinationLocation } = useLocationStore();
 
   const handleSignOut = () => {
     signOut();
     router.replace("/(auth)/sign-in");
   };
 
-  const handleDestinationPress = (location: {
-    latitude: number;
-    longitude: number;
-    address: string;
-  }) => {
-    setDestinationLocation(location);
+  const [hasPermission, setHasPermission] = useState<boolean>(false);
 
-    router.push("/(root)/find-ride");
-  };
-
-  const recentRides = [
-    {
-      ride_id: "1",
-      origin_address: "Kathmandu, Nepal",
-      destination_address: "Pokhara, Nepal",
-      origin_latitude: 27.717245,
-      origin_longitude: 85.323961,
-      destination_latitude: 28.209583,
-      destination_longitude: 83.985567,
-      ride_time: 391,
-      fare_price: 19500.0,
-      payment_status: "paid",
-      driver_id: 2,
-      user_id: "1",
-      created_at: "2024-08-12 05:19:20.620007",
-      driver: {
-        first_name: "David",
-        last_name: "Brown",
-        car_seats: 5,
-      },
-    },
-    {
-      ride_id: "2",
-      origin_address: "Jalkot, MH",
-      destination_address: "Pune, Maharashtra, India",
-      origin_latitude: 18.609116,
-      origin_longitude: 77.165873,
-      destination_latitude: 18.52043,
-      destination_longitude: 73.856744,
-      ride_time: 491,
-      fare_price: 24500.0,
-      payment_status: "paid",
-      driver_id: 1,
-      user_id: "1",
-      created_at: "2024-08-12 06:12:17.683046",
-      driver: {
-        first_name: "James",
-        last_name: "Wilson",
-        car_seats: 4,
-      },
-    },
-    {
-      ride_id: "3",
-      origin_address: "Zagreb, Croatia",
-      destination_address: "Rijeka, Croatia",
-      origin_latitude: 45.815011,
-      origin_longitude: 15.981919,
-      destination_latitude: 45.327063,
-      destination_longitude: 14.442176,
-      ride_time: 124,
-      fare_price: 6200.0,
-      payment_status: "paid",
-      driver_id: 1,
-      user_id: "1",
-      created_at: "2024-08-12 08:49:01.809053",
-      driver: {
-        first_name: "James",
-        last_name: "Wilson",
-        car_seats: 4,
-      },
-    },
-    {
-      ride_id: "4",
-      origin_address: "Okayama, Japan",
-      destination_address: "Osaka, Japan",
-      origin_latitude: 34.655531,
-      origin_longitude: 133.919795,
-      destination_latitude: 34.693725,
-      destination_longitude: 135.502254,
-      ride_time: 159,
-      fare_price: 7900.0,
-      payment_status: "paid",
-      driver_id: 3,
-      user_id: "1",
-      created_at: "2024-08-12 18:43:54.297838",
-      driver: {
-        first_name: "Michael",
-        last_name: "Johnson",
-        car_seats: 4,
-      },
-    },
-  ];
+  const {
+    data: recentRides,
+    loading,
+    error,
+  } = useFetch<Ride[]>(`/(api)/ride/${user?.id}`);
 
   useEffect(() => {
     (async () => {
@@ -145,6 +62,16 @@ export default function Page() {
       });
     })();
   }, []);
+
+  const handleDestinationPress = (location: {
+    latitude: number;
+    longitude: number;
+    address: string;
+  }) => {
+    setDestinationLocation(location);
+
+    router.push("/(root)/find-ride");
+  };
 
   return (
     <SafeAreaView className="bg-general-500">
@@ -182,11 +109,12 @@ export default function Page() {
               </Text>
               <TouchableOpacity
                 onPress={handleSignOut}
-                className="justify-center items-center w-10 h-10 rounded-full bg-red-200"
+                className="justify-center items-center w-10 h-10 rounded-full bg-white"
               >
                 <Image source={icons.out} className="w-4 h-4" />
               </TouchableOpacity>
             </View>
+
             <GoogleTextInput
               icon={icons.search}
               containerStyle="bg-white shadow-md shadow-neutral-300"
@@ -210,4 +138,6 @@ export default function Page() {
       />
     </SafeAreaView>
   );
-}
+};
+
+export default Home;
